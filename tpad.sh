f1cbaba5856dd914acbb03ed9b79261b2b2de18c
GabrielAuger1 <gauger@edu.cegepgarneau.ca>
2024-06-13 15:11:16 -0400

    update comments

--- tpad.sh
+++ tpad.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-tpadid=$(xinput list | grep "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1)
+# This script simply fetch the xinput information on the device named "Touchpad" and then toggle the device according to its status.
 
+
+tpadid=$(xinput list | grep "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1)
 tpadcode=$(xinput list-props $tpadid | grep "Device Enabled" | grep "Device Enabled" | cut -d'(' -f2 | cut -d')' -f1)
 
-#fetch the number in the parenthesis after "Device Enabled"
-#check if touchpad is enabled or disabled with tpadcode
-
 tpadEnabled=$(xinput list-props $tpadid | grep "Enabled ($tpadcode)" | grep "Enabled ($tpadcode)")
-
-#get last character of var
 tpadEnabled=${tpadEnabled: -1}
-
-
-#check if last character of grep is 1
-
-
 
 if [ $tpadEnabled -eq 1 ]; then
     xinput set-prop $tpadid $tpadcode 0
